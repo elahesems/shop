7bbfd832566403c90e03f8ffb6f07bc7a94f5db6
elahe sems <olenasems88@gmail.com>
2020-11-30 22:20:31 +0300

    first commit

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web',
+    'multiselectfield',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
